f0bcb3c2029d7d1954d3aaf4a264901f24ec0e68
Rome Bell <romebell5@gmail.com>
2020-12-18 15:46:20 -0800

    Update REACT_APP_SERVER_URL to original

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,9 +2,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
-// const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
-import keys from '../utils/credentials';
-const { REACT_APP_SERVER_URL } = keys;
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+// import keys from '../utils/credentials';
+// const { REACT_APP_SERVER_URL } = keys;
 
 const Signup = () => {
     const [name, setName] = useState('');
